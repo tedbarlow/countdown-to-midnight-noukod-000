48ae8eb1044012a12b061ae70b505b40b9d6b29b
Ted Barlow <ted@noukod.com>
2018-12-05 18:50:32 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number > 0 do
+    puts "#{number} SECOND(S)!"
+    number-=1
+  end
+  countdown = "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  sleep(number)
 end
